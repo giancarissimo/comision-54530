167f9eb73718e849947482a94d529c6b89c2a478
giancarissimo <giancarissimo@gmail.com>
2023-05-23 00:28:49 -0300

    añadi menu hamburguesa (js) + media query

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.button-menu')
+const nav = document.querySelector ('.nav')
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('active')
+    button.classList.toggle('open')
+})
